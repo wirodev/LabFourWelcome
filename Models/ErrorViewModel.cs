07312a479a80449bffde4eb17914704f1a9d02f8
Robin Wiman <X00204927@myTUDublin.ie>
2024-03-26 10:27:09 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LabFourWelcome.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
